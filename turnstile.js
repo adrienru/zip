f6d820afa510135b65768bdb39c6517da83af9a9
adrienru <u25253967@utp.edu.pe>
2025-04-06 18:07:07 -0600

    Create turnstile.js

---
+++ turnstile.js
@@ -0,0 +1,14 @@
+// turnstile.js
+
+window.onloadTurnstileCallback = function () {
+  turnstile.render("#example-container", {
+    sitekey: "0x4AAAAAABE79aVbG0qQSWTT,
+    callback: function (token) {
+      console.log(`Challenge Success ${token}`);
+      // Aquí puedes enviar el token al servidor para validarlo
+    },
+    errorCallback: function (error) {
+      console.log("Error en el desafío de Turnstile:", error);
+    }
+  });
+};
